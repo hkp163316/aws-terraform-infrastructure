7177b7086c2177a627dae1937f6270b0e5a74952
Hiren Patel <154289095+hkp163316@users.noreply.github.com>
2025-05-21 15:16:48 -0500

    Added terraform/variables.tf for input variables

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  default     = "hkp16-terraform-bucket-hp-2025"
+}
