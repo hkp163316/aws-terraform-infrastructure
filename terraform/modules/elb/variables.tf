7072fe1042a75c1d292805fd6fcea0aff696e179
Hiren Patel <154289095+hkp163316@users.noreply.github.com>
2025-05-21 16:40:27 -0500

    Added terraform/modules/elb/variables.tf for ELB variables

---
+++ terraform/modules/elb/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  description = "VPC ID for the ELB"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the ELB"
+}
+
+variable "ec2_instance_id" {
+  description = "EC2 instance ID for the ELB"
+}
